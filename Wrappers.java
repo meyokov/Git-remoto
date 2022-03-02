bc9f22bfeab21387fd1b744ff6913dd1e1ec835f
yMaduv <78622984+yMaduv@users.noreply.github.com>
2022-03-02 04:24:00 -0300

    Via upload
    Estudo de wrappers.

---
+++ Wrappers.java
@@ -0,0 +1,22 @@
+public class Wrappers {
+    public static void main (String[] args) {
+
+        Byte byteW = 111;
+        Short shortW = 2;
+        Integer intW = 90;
+        Long longW = 256168516L;
+        Float floatW = 1451F;
+        Double doubleW = 50D;
+        Character charW = 'Y';
+        Boolean booleanW = true;
+
+        System.out.println(byteW);
+        System.out.println(shortW);
+        System.out.println(intW);
+        System.out.println(longW);
+        System.out.println(floatW);
+        System.out.println(doubleW);
+        System.out.println(charW);
+        System.out.println(booleanW);
+    }
+}
